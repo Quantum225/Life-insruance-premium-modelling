139e4a2f755faa6345bb4404f75c5b95be80ec13
Quantum225 <judelow2005@gmail.com>
2025-08-10 20:29:06 +0800

    Update Life_premiums2v2.R

--- Life_premiums2.R
+++ Life_premiums2.R
@@ -1,6 +1,10 @@
 #Parameters(3):~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 MortR=c(0.004521, 0.000244, 0.000165, 0.000109, 0.000083, 0.000086, 0.00007, 0.000072, 0.000076, 0.000071, 0.000059, 0.000095, 0.000102, 0.000118, 0.000132, 0.000178, 0.00024, 0.00031, 0.000434, 0.000507, 0.000479, 0.000514, 0.00052, 0.000582, 0.000546, 0.000544, 0.000664, 0.000643, 0.000699, 0.000782, 0.000795, 0.000886, 0.000928, 0.000992, 0.00103, 0.001129, 0.00127, 0.001342, 0.001446, 0.001616, 0.001731, 0.001871, 0.00197, 0.002115, 0.002275, 0.002503, 0.00268, 0.002936, 0.003262, 0.003519, 0.003822, 0.004054, 0.004486, 0.004692, 0.005087, 0.005438, 0.005867, 0.006409, 0.00686, 0.007387, 0.008134, 0.009036, 0.009714, 0.010535, 0.0116, 0.012545, 0.013886, 0.015413, 0.016882, 0.018186, 0.019951, 0.021931, 0.024026, 0.025858, 0.028047, 0.031242, 0.035063, 0.038891, 0.043968, 0.048651, 0.055679, 0.062008, 0.070065, 0.076758, 0.086004, 0.096018, 0.107573, 0.120841, 0.136178, 0.151392, 0.170648, 0.187061, 0.209933, 0.229476, 0.252988, 0.276409, 0.297662, 0.32832, 0.339591, 0.378378, 0.401956)
-#Mortality rate for ages 1-101 i.e. q1,q2,...,q101 from ONS mortality rate
+#Mortality rate for ages 1-101 i.e. q1,q2,...,q101
+#must format so that the first position in the vector represents the mortality
+#rate for going from 0 to 1 year old, position 2 for rate going from 1 to 2 yr old etc.
+#The data in use currently is from the ONS, the mortality rates for males in the UK
+
 RI=c(rep(0.98,69),rep(0.982,5),rep(0.985,5),rep(0.986,5),rep(0.992,10),rep(0.995,7))
 #Rate of improvement for each age, i.e. r1,r2,...,r101
 #For now these are placeholders, these would need to be calculated/estimated from past data
@@ -23,9 +27,11 @@ for (n in 1:(length(RI)-age)) {
 
 AdMortR=1
 #We want to create vector, c(q71*r71,q72*r72^2,...,q101*r101^31)
-AdMortR=RI2*MortR[(age+1):101]
+AdMortR=c(RI2*MortR[(age+1):101],1)
 AdMortR
 #This represents the adjusted mortality rate for a person of stated age
+#A one is added in the end to make sure that a person doesn't just live forever
+#Not enough data, so assume person dies after year 101
 
 
 #PMF & CDF~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -39,15 +45,15 @@ if (n==1) NULL else neg[(age+1):(age+n-1)]
 #This indexes c(1-q71r71,....,1-q(70+n)r(70+n)^n), NULL if n=1
 
 PMF=1 #initialise
-for (n in 1:(101-age)) {
+for (n in 1:length(AdMortR)) {
   PMF[n]=AdMortR[n]*prod(if (n<=1) NULL else neg[1:(n-1)])
 }
 #Calculates PMF where PMF[n] holds the probability of dying in (n-1,n] years
-PMF[102-age]=prod(neg)
-#Inputs the last value in PMF to represent P[x>101], as we don't have mortality rates past this age
+
+
 
 CDF=1
-for (n in 1:(102-age)) {
+for (n in 1:length(PMF)) {
   CDF[n]=sum(PMF[1:n])
 }
 #Calculates CDF from PDF
@@ -56,10 +62,10 @@ for (n in 1:(102-age)) {
 #Barplots~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 barplot(CDF,space=0,main='CDF',ylab='p',xlab='years left') #plots CDF
-axis(1,at=c(0:(102-age)),cex.axis=0.4)
+axis(1,at=0:length(CDF),cex.axis=0.5)
 
 barplot(PMF,space=0,main='PMF',ylab='p',xlab='years left') #plots PMF
-axis(1,at=c(0:(102-age)),cex.axis=0.5)
+axis(1,at=0:length(PMF),cex.axis=0.5)
 #Note that the final bar for reaching age 102 is higher than age 101, that is because
 #the last bar represents the probability of reaching age 102+ not just 102
 #The later calculations will consider this as 102, not 102+ though
@@ -67,10 +73,9 @@ axis(1,at=c(0:(102-age)),cex.axis=0.5)
 
 #Percentiles & Mean~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-ExEx=sum(c(seq(age+1,101),102)*PMF)-age-0.5
+ExEx=sum(seq(age+1,length(MortR)+1)*PMF)-age
 ExEx
 #This calculates expected life expectancy, i.e. the mean years left to live
-#-0.5 to adjust for the fact that person doesn't die in exactly n years but lies in (n-1,n]
 
 q=0.95
 which(CDF>=q)[1]
@@ -97,7 +102,7 @@ length(PMF)
 i=0.05
 v=1/(1-i)
 DR=1
-for (n in 1:(102-age)) {
+for (n in 1:length(AdMortR)) {
   DR[n]=v^n
 }
 DR
@@ -105,6 +110,7 @@ DR
 
 neg2=c(1,1-CDF[1:(length(CDF)-1)]) #This vector represents probability of surviving [n] years
 neg2
+CDF
 
 EPVb=sum(DR*PMF)
 #Expected present value of the benefits where benefits is $1 awarded at death
